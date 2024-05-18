305b8ddeb4750033b3e6ce66eb439e883bdeea5d
Erik Araujo <earaujo@easyjur.com>
2024-05-18 18:49:37 -0300

    adicao campo de busca ao search

--- 07 - Loja Quadrinhos/script.js
+++ 07 - Loja Quadrinhos/script.js
@@ -3,17 +3,24 @@ $(document).ready(function () {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
-
 });
 
-$(document).on('DOMContentLoaded', function () {
-    new Splide('.splide', {
-      type: 'slide',
-      perPage: 1,
-      autoplay: true,
-      pauseOnHover: true,
-      arrows: true,
-      pagination: false,
-    }).mount();
-  });
+$(document).on("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "loop",
+    perPage: 1,
+    autoplay: true,
+    pauseOnHover: true,
+    arrows: true,
+    pagination: false,
+    loop: true,
+  }).mount();
+});
 
+$("#busca_quadrinho").on("input", function () {
+  if ($(this).val().length > 0) {
+    $("#card_de_busca").removeClass("is-hidden");
+  } else {
+    $("#card_de_busca").addClass("is-hidden");
+  }
+});
