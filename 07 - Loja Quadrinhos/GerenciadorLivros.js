acb807d05a88b2dd9d59b54c6b5474197758a07e
GabrielPw <gxaavier352@gmail.com>
2024-05-18 22:37:38 -0300

    filtragem de livros

---
+++ 07 - Loja Quadrinhos/GerenciadorLivros.js
@@ -0,0 +1,18 @@
+class GerenciadorLivros {
+    constructor(livros) {
+        this.livros = livros;
+    }
+
+    filtrarPorTitulo(titulo){
+        return this.livros.filter(livro => livro.nome.toLowerCase().includes(titulo.toLowerCase()));
+    }
+
+    filtrarPorAutor(autor) {
+        return this.livros.filter(livro => livro.autor === autor);
+    }
+
+    filtrarPorEditora(editora) {
+        return this.livros.filter(livro => livro.editora === editora);
+    }
+
+}
---
+++ 07 - Loja Quadrinhos/Livro.js
@@ -0,0 +1,11 @@
+class Livro{
+
+    constructor(nome, autor, editora, imageUrl, linkUrl){
+
+        this.nome = nome;
+        this.autor = autor;
+        this.editora = editora;
+        this.imageUrl = imageUrl;
+        this.linkUrl = linkUrl;
+    }
+}
